aa31aa2f34283185348c364a2c7a2b71a1a00cd5
chenbw <opt_cbw@hotmail.com>
2021-05-16 21:33:56 +0800

    update leetcode weekly

---
+++ leetcode/1269. Number of Ways to Stay in the Same Place After Some Steps.py
@@ -0,0 +1,54 @@
+# You have a pointer at index 0 in an array of size arrLen. At each step, you can move 1 position to the left,
+# 1 position to the right in the array, or stay in the same place (The pointer should not be placed outside the array
+# at any time).
+# 
+# Given two integers steps and arrLen, return the number of ways such that your pointer still at index 0 after
+# exactly steps steps. Since the answer may be too large, return it modulo 109 + 7.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: steps = 3, arrLen = 2
+# Output: 4
+# Explanation: There are 4 differents ways to stay at index 0 after 3 steps.
+# Right, Left, Stay
+# Stay, Right, Left
+# Right, Stay, Left
+# Stay, Stay, Stay
+# Example 2:
+# 
+# Input: steps = 2, arrLen = 4
+# Output: 2
+# Explanation: There are 2 differents ways to stay at index 0 after 2 steps
+# Right, Left
+# Stay, Stay
+# Example 3:
+# 
+# Input: steps = 4, arrLen = 2
+# Output: 8
+#  
+# 
+# Constraints:
+# 
+# 1 <= steps <= 500
+# 1 <= arrLen <= 106
+
+# 递归改动态规划。有几个可变参数决定你维护的dp数组是几维数组
+class Solution:
+    def numWays(self, steps: int, arrLen: int) -> int:
+        mod = 10 ** 9 + 7
+        max_j = min(steps, arrLen - 1)
+
+        dp = [[0] * (max_j + 1) for _ in range(steps + 1)]  # * ( steps + 1 )
+        dp[0][0] = 1
+
+        for i in range(1, steps + 1):
+            for j in range(0, max_j + 1):
+                dp[i][j] = dp[i - 1][j]
+                if j - 1 >= 0:
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % mod
+                if j + 1 <= max_j:
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % mod
+
+        return dp[steps][0]
