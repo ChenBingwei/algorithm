00e3acc1b86d437937c67585a63297855091c070
chenbw <chenbw@fiberhome.com>
2021-05-18 21:07:48 +0800

    binary tree

--- tree/BinaryTreeInit.py
+++ tree/BinaryTreeInit.py
@@ -1,6 +1,3 @@
-from time import time
-
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -42,26 +39,5 @@ def stringToTreeNode(input):
     return root
 
 
-def print_time(func):
-    def wrappers(*args, **kwargs):
-        start = time()
-        print('func: {}'.format(func.__name__))
-        print('result: {}'.format(func(*args, **kwargs)))
-        end = time()
-        print("spent time:%.4f" % ((end - start) * 1000))
-        print("***************")
-
-    return wrappers
-
-
-def exchange_maskint(mask_int):
-    bin_arr = ['0' for i in range(32)]
-    for i in range(mask_int):
-        bin_arr[i] = '1'
-    tmp_mask = [''.join(bin_arr[i * 8:i * 8 + 8]) for i in range(4)]
-    tmp_mask = [str(int(tmp_str, 2)) for tmp_str in tmp_mask]
-    return '.'.join(tmp_mask)
-
-
 if __name__ == '__main__':
-    print(exchange_maskint(8))
+    stringToTreeNode('[1,2,3]')
